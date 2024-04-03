b9c345bae68bdd1a665ea2d98a62204a7bbcc712
dipakn491 <dipakn491@gmail.com>
2024-04-03 22:01:33 +0530

    added all things

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -1,141 +1,141 @@
-// (function() {
-//   // get all data in form and return object
-//   function getFormData(form) {
-//     var elements = form.elements;
-//     var honeypot;
+(function() {
+  // get all data in form and return object
+  function getFormData(form) {
+    var elements = form.elements;
+    var honeypot;
 
-//     var fields = Object.keys(elements).filter(function(k) {
-//       if (elements[k].name === "honeypot") {
-//         honeypot = elements[k].value;
-//         return false;
-//       }
-//       return true;
-//     }).map(function(k) {
-//       if(elements[k].name !== undefined) {
-//         return elements[k].name;
-//       // special case for Edge's html collection
-//       }else if(elements[k].length > 0){
-//         return elements[k].item(0).name;
-//       }
-//     }).filter(function(item, pos, self) {
-//       return self.indexOf(item) == pos && item;
-//     });
+    var fields = Object.keys(elements).filter(function(k) {
+      if (elements[k].name === "honeypot") {
+        honeypot = elements[k].value;
+        return false;
+      }
+      return true;
+    }).map(function(k) {
+      if(elements[k].name !== undefined) {
+        return elements[k].name;
+      // special case for Edge's html collection
+      }else if(elements[k].length > 0){
+        return elements[k].item(0).name;
+      }
+    }).filter(function(item, pos, self) {
+      return self.indexOf(item) == pos && item;
+    });
 
-//     var formData = {};
-//     fields.forEach(function(name){
-//       var element = elements[name];
+    var formData = {};
+    fields.forEach(function(name){
+      var element = elements[name];
       
-//       // singular form elements just have one value
-//       formData[name] = element.value;
+      // singular form elements just have one value
+      formData[name] = element.value;
 
-//       // when our element has multiple items, get their values
-//       if (element.length) {
-//         var data = [];
-//         for (var i = 0; i < element.length; i++) {
-//           var item = element.item(i);
-//           if (item.checked || item.selected) {
-//             data.push(item.value);
-//           }
-//         }
-//         formData[name] = data.join(', ');
-//       }
-//     });
+      // when our element has multiple items, get their values
+      if (element.length) {
+        var data = [];
+        for (var i = 0; i < element.length; i++) {
+          var item = element.item(i);
+          if (item.checked || item.selected) {
+            data.push(item.value);
+          }
+        }
+        formData[name] = data.join(', ');
+      }
+    });
 
-//     // add form-specific values into the data
-//     formData.formDataNameOrder = JSON.stringify(fields);
-//     formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-//     formData.formGoogleSendEmail
-//       = form.dataset.email || ""; // no email by default
+    // add form-specific values into the data
+    formData.formDataNameOrder = JSON.stringify(fields);
+    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    formData.formGoogleSendEmail
+      = form.dataset.email || ""; // no email by default
 
-//     return {data: formData, honeypot: honeypot};
-//   }
-
-//   function handleFormSubmit(event) {  // handles form submit without any jquery
-//     event.preventDefault();           // we are submitting via xhr below
-//     var form = event.target;
-//     var formData = getFormData(form);
-//     var data = formData.data;
-
-//     // If a honeypot field is filled, assume it was done so by a spam bot.
-//     if (formData.honeypot) {
-//       return false;
-//     }
-
-//     disableAllButtons(form);
-//     var url = form.action;
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('POST', url);
-//     // xhr.withCredentials = true;
-//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//           form.reset();
-//           var formElements = form.querySelector(".form-elements")
-//           if (formElements) {
-//             formElements.style.display = "none"; // hide form
-//           }
-//           var thankYouMessage = form.querySelector(".thankyou_message");
-//           if (thankYouMessage) {
-//             thankYouMessage.style.display = "block";
-//           }
-//         }
-//     };
-//     // url encode form data for sending as post data
-//     var encoded = Object.keys(data).map(function(k) {
-//         return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-//     }).join('&');
-//     xhr.send(encoded);
-//   }
-  
-//   function loaded() {
-//     // bind to the submit event of our form
-//     var forms = document.querySelectorAll("form.gform");
-//     for (var i = 0; i < forms.length; i++) {
-//       forms[i].addEventListener("submit", handleFormSubmit, false);
-//     }
-//   };
-//   document.addEventListener("DOMContentLoaded", loaded, false);
-
-//   function disableAllButtons(form) {
-//     var buttons = form.querySelectorAll("button");
-//     for (var i = 0; i < buttons.length; i++) {
-//       buttons[i].disabled = true;
-//     }
-//   }
-// })();
-
-
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  var form = event.target;
-  var formData = getFormData(form);
-  
-  if (formData.honeypot) {
-    return false;
+    return {data: formData, honeypot: honeypot};
   }
 
-  disableAllButtons(form);
-  var url = 'http://localhost:3000/submit-form'; // Change this to match your proxy server URL
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        form.reset();
-        var formElements = form.querySelector(".form-elements")
-        if (formElements) {
-          formElements.style.display = "none"; // hide form
+  function handleFormSubmit(event) {  // handles form submit without any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var form = event.target;
+    var formData = getFormData(form);
+    var data = formData.data;
+
+    // If a honeypot field is filled, assume it was done so by a spam bot.
+    if (formData.honeypot) {
+      return false;
+    }
+
+    disableAllButtons(form);
+    var url = form.action;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          form.reset();
+          var formElements = form.querySelector(".form-elements")
+          if (formElements) {
+            formElements.style.display = "none"; // hide form
+          }
+          var thankYouMessage = form.querySelector(".thankyou_message");
+          if (thankYouMessage) {
+            thankYouMessage.style.display = "block";
+          }
         }
-        var thankYouMessage = form.querySelector(".thankyou_message");
-        if (thankYouMessage) {
-          thankYouMessage.style.display = "block";
-        }
-      } else {
-        console.error('Error:', xhr.statusText);
-      }
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+    }).join('&');
+    xhr.send(encoded);
+  }
+  
+  function loaded() {
+    // bind to the submit event of our form
+    var forms = document.querySelectorAll("form.gform");
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].addEventListener("submit", handleFormSubmit, false);
     }
   };
-  xhr.send(JSON.stringify(formData.data));
-}
+  document.addEventListener("DOMContentLoaded", loaded, false);
+
+  function disableAllButtons(form) {
+    var buttons = form.querySelectorAll("button");
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+  }
+})();
+
+
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+//   var form = event.target;
+//   var formData = getFormData(form);
+  
+//   if (formData.honeypot) {
+//     return false;
+//   }
+
+//   disableAllButtons(form);
+//   var url = 'http://localhost:3000/submit-form'; // Change this to match your proxy server URL
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('POST', url);
+//   xhr.setRequestHeader("Content-Type", "application/json");
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         form.reset();
+//         var formElements = form.querySelector(".form-elements")
+//         if (formElements) {
+//           formElements.style.display = "none"; // hide form
+//         }
+//         var thankYouMessage = form.querySelector(".thankyou_message");
+//         if (thankYouMessage) {
+//           thankYouMessage.style.display = "block";
+//         }
+//       } else {
+//         console.error('Error:', xhr.statusText);
+//       }
+//     }
+//   };
+//   xhr.send(JSON.stringify(formData.data));
+// }
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const axios = require('axios');
+// const bodyParser = require('body-parser');
 
-const app = express(); // Initialize the app variable first
-const PORT = process.env.PORT || 3000;
+// const app = express(); // Initialize the app variable first
+// const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public')); // Set up static file serving middleware
-app.use(bodyParser.json()); // Use bodyParser middleware to parse JSON request bodies
+// app.use(express.static('public')); // Set up static file serving middleware
+// app.use(bodyParser.json()); // Use bodyParser middleware to parse JSON request bodies
 
-// Define your route after initializing the app variable
-app.post('/submit-form', async (req, res) => {
-  try {
-    const response = await axios.post('https://script.google.com/macros/s/AKfycbyz5f4t6aRP_s9D-3tgh7iLfoM083255oQIRM8L0TuNLubrJ9zKcjKpXhx2Yudy7AWC/exec', req.body);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+// // Define your route after initializing the app variable
+// app.post('/submit-form', async (req, res) => {
+//   try {
+//     const response = await axios.post('https://script.google.com/macros/s/AKfycbyz5f4t6aRP_s9D-3tgh7iLfoM083255oQIRM8L0TuNLubrJ9zKcjKpXhx2Yudy7AWC/exec', req.body);
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
